ecde71a94c33592485ad7847e9e6a8d815dd2e34
Aesha <aeshagadhiya04@gmail.com>
2024-04-27 15:43:32 +0530

    width height changed of images

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -319,7 +319,7 @@ export default function CreateListing() {
           <div className="flex gap-4">
             <input
               onChange={(e) => setFiles(e.target.files)}
-              className="p-3 border border-gray-300 rounded w-full"
+              className="p-3 border border-gray-300 rounded w-100% h-auto"
               type="file"
               id="images"
               accept="image/*"
