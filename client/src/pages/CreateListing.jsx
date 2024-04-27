0ff5d7c145ef4de7db45d7dfa3f615b63aa75968
Aesha <aeshagadhiya04@gmail.com>
2024-04-27 15:47:49 +0530

    w,h chnaged

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -319,7 +319,7 @@ export default function CreateListing() {
           <div className="flex gap-4">
             <input
               onChange={(e) => setFiles(e.target.files)}
-              className="p-3 border border-gray-300 rounded w-1000px h-700px"
+              className="p-3 border border-gray-300 rounded w-100% h-100p%"
               type="file"
               id="images"
               accept="image/*"
