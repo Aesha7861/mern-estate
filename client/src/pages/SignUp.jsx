21af801336f64f6ef30ab3ecaf43c01a74543f86
Aesha <aeshagadhiya04@gmail.com>
2024-05-01 22:44:39 +0530

    Style changed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -49,7 +49,7 @@ const handleSubmit = async (e) => {
 
     <div className='p-3 max-w-lg mx-auto'> 
 
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7 text-blue-900'>Sign Up</h1>
 
       <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
         <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
